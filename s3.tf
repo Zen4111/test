d025213c707a64ac804c27e4ca8ed53f89db51a0
Collins Orock <97584088+collinsako@users.noreply.github.com>
2023-02-18 12:15:45 -0500

    Delete s3.tf

--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bucklebuck" {
-  bucket = "my-tf-massiveprince-bucket11"
-  acl    = "private"
-
-  tags = {
-    Name        = "rav1i-day1a-santhos1h-deepak1231"
-    Environment = "Dev"
-  }
-}
